24c9f1e7aabae679ccdc5d27b0b962c993a78e13
JesseNReynolds <jessenreynolds@gmail.com>
2021-01-08 12:35:03 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const list = document.getElementById('list')
+
+  document.getElementById('submit-button-overwrite').addEventListener('click', function(event){
+    // prevent default behavior
+    event.preventDefault();
+    // get the string from the text-field
+    let textFromForm = document.getElementById('new-task-description').value;
+    //  create and insert the new LI
+    let newTask = document.createElement('li');
+    let deleteButton = '<button onclick="return this.parentNode.remove();">delete</button>'
+    list.appendChild(newTask);
+    // set the new LI's text
+    newTask.innerHTML = `${textFromForm} ${deleteButton}`;
+    // empty the text field
+    document.getElementById('new-task-description').value = '';
+  });
+
 });
